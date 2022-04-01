d14fa32e2c5737aa20e6e8ea4777cddb7b37ffaf
Nikita Aksakov <artcodefun@gmail.com>
2022-04-01 22:45:09 +0300

    HomePage added

---
+++ lib/components/DayCard.dart
@@ -0,0 +1,50 @@
+import 'package:endless_note/models/DayNotes.dart';
+import 'package:flutter/material.dart';
+import 'dart:math';
+
+class DayCard extends StatelessWidget {
+  const DayCard({Key? key, required this.dayNotes}) : super(key: key);
+
+  final DayNotes dayNotes;
+
+  String dateDisplay(DateTime d) => "${d.year}/${d.month}/${d.day}";
+
+  Color randColor(Random r) {
+    Color c;
+    do{
+    c = Color.fromARGB(
+        255, r.nextInt(256), r.nextInt(256), r.nextInt(256));}
+    while(c.computeLuminance()<0.5);
+
+    return c;
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    var theme = Theme.of(context);
+    var r = Random(dayNotes.date.millisecondsSinceEpoch);
+    return Container(
+      decoration: BoxDecoration(
+          image: DecorationImage(
+              image: AssetImage("assets/patterns/${r.nextInt(18) + 1}.png"),
+              repeat: ImageRepeat.repeat),
+          boxShadow: [
+            BoxShadow(
+                color: Colors.black.withOpacity(0.3),
+                offset: Offset(0, 5),
+                blurRadius: 10)
+          ],
+          borderRadius: BorderRadius.circular(25),
+          color: randColor(r)),
+      margin: const EdgeInsets.all(10),
+      height: 200,
+      child: Column(mainAxisAlignment: MainAxisAlignment.center, children: [
+        Text(
+          dateDisplay(dayNotes.date),
+          style: theme.textTheme.headline4?.copyWith(
+              color: theme.colorScheme.onSurface, fontWeight: FontWeight.w600),
+        )
+      ]),
+    );
+  }
+}
---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,33 @@
+import 'package:endless_note/components/DayCard.dart';
+import 'package:endless_note/models/DayNotes.dart';
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+  const HomePage({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    var now = DateTime.now();
+    var today = DateTime(now.year, now.month, now.day);
+    return Scaffold(
+      appBar: AppBar(
+        titleSpacing: 0,
+        title: const SizedBox(
+          width: double.maxFinite,
+          child: Image(
+            repeat: ImageRepeat.repeat,
+            alignment: Alignment.center,
+            image: AssetImage('assets/images/gz.jpg'),
+            width: 200,
+            height: 200,
+          ),
+        ),
+      ),
+      body: ListView.builder(
+          itemBuilder: (ctx, i) => DayCard(
+              dayNotes: DayNotes(
+                  notes: [],
+                  date: today.subtract(Duration(days: i * 1))))),
+    );
+  }
+}
