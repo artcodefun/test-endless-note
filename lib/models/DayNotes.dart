7a4f73f6969c7dc079085b554d5e79360f77d87c
Nikita Aksakov <artcodefun@gmail.com>
2022-04-02 18:18:22 +0300

    new useful methods for models

--- lib/models/DayNotes.dart
+++ lib/models/DayNotes.dart
@@ -12,10 +12,21 @@ class DayNotes{
   DayNotes copyWith({
     DateTime? date,
     Map<int,Note>? notes,
+    bool makeNewMap =false
   }) {
+    Map<int,Note> newNotes = notes ?? this.notes;
+
+    if(makeNewMap){
+      var m = newNotes;
+      newNotes = {};
+      m.forEach((key, value) {
+        newNotes[key]=value;
+      });
+    }
+
     return DayNotes(
       date: date ?? this.date,
-      notes: notes ?? this.notes,
+      notes: newNotes,
     );
   }
 }
--- lib/models/Note.dart
+++ lib/models/Note.dart
@@ -5,11 +5,25 @@ class Note{
     required this.text,
   });
 
+
+
   Note copyWith({
     String? text,
   }) {
     return Note(
       text: text ?? this.text,
+    );
+  }
+
+  Map<String, dynamic> toMap() {
+    return {
+      'text': this.text,
+    };
+  }
+
+  factory Note.fromMap(Map<String, dynamic> map) {
+    return Note(
+      text: map['text'] as String,
     );
   }
 }
